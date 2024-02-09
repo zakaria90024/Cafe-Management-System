08ed085abaa88a7df1d34b101ea42e392b688f76
Zakaria <zakaria112203@gmail.com>
2024-02-09 23:27:16 +0600

    JWT Bearer Token Integration Successfully. change password forget password category post/get_all/update product  post/get_all/update_with_id/delete_id BillReport.pdf Write with Document

--- src/main/java/com/sasoftbd/cafesystem/Cafe/Management/System/constents/CafeConstants.java
+++ src/main/java/com/sasoftbd/cafesystem/Cafe/Management/System/constents/CafeConstants.java
@@ -4,5 +4,5 @@ public class CafeConstants {
     public static final String SOMETHING_WANT_WRONG = "Something Went Wrong";
     public static final  String INVALID_DATA = "Invalid Data";
     public static final  String UNAUTHORIZED_ACCESS = "Unauthorized Access";
-    public static final String STORE_LOCATION = "C:\\Users\\Zakaria\\Documents\\CAFE\\Cafe-Management-System";
+    public static final String STORE_LOCATION = "C:\\Users\\Zakaria\\Documents\\CAFE\\Cafe-Management-System\\billpdf";
 }
