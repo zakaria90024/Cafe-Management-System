dfde4962d0c2a9229f71eb8b133163943ba98df4
Zakaria <zakaria112203@gmail.com>
2024-02-10 23:29:57 +0600

    JWT Bearer Token Integration Successfully. change password forget password category post/get_all/update product  post/get_all/update_with_id/delete_id BillReport.pdf Write with Document
    angular error solve
    https://nodejs.org/en/blog/release/v14.15.4
    download node specific version
    npm uninstall -g @angular/cli
    npm cache clean or npm cache verify
    npm install -g @angular/cli@wished.version.here

---
+++ Frontend/src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url = environment.apiUrl
+  constructor(private httpClient:HttpClient) {}
+  signup(data:any){
+    return this.httpClient.post(this.url+"user/singup", data, {
+      headers:new HttpHeaders().set('Content-Type', 'application/json')
+    })
+  }
+}
