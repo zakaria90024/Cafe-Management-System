6d424e640709203f1c8b28c9c0256d2d15d4e1dc
Zakaria <zakaria112203@gmail.com>
2024-02-20 23:35:13 +0600

    JWT Bearer Token Integration Successfully. change password forget password category post/get_all/update product  post/get_all/update_with_id/delete_id BillReport.pdf Write with Document
    angular error solve
    https://nodejs.org/en/blog/release/v14.15.4
    download node specific version
    npm uninstall -g @angular/cli
    npm cache clean or npm cache verify
    npm install -g @angular/cli@wished.version.here
    
    //sinup page with angular frontenduI java

--- src/main/java/com/sasoftbd/cafesystem/Cafe/Management/System/dao/UserDao.java
+++ src/main/java/com/sasoftbd/cafesystem/Cafe/Management/System/dao/UserDao.java
@@ -13,9 +13,7 @@ import java.util.List;
 public interface UserDao extends JpaRepository<User, Integer> {
 
     User findByEmailId(@Param("email") String email);
-
     List<UserWrapper> getAllUser();
-
     List<String> getAllAdmin();
 
     @Transactional
